dc8298a20e49f887000c6992f043b1066b90f31a
Agustin <agushoj@gmail.com>
2023-01-28 23:17:59 -0300

    TP Terminado

---
+++ TP JS.js
@@ -0,0 +1,16 @@
+function CambiarAContactos(){
+    document.getElementById("titulo1").innerHTML = "Contacto:";
+    
+    document.getElementById("fecha").innerHTML = "11 3521-5612";
+    document.getElementById("dni").innerHTML = "williamstep@gmail.com";
+    document.getElementById("dire").innerHTML = "www.linkedin.com/in/wstephens";
+}
+
+
+
+function CambiarAInfo(){
+    document.getElementById("titulo1").innerHTML = "Información:";
+    document.getElementById("fecha").innerHTML = "Nacimiento: 12/3/1992";
+    document.getElementById("dni").innerHTML = "DNI: 41782834";
+    document.getElementById("dire").innerHTML = "Dirección: Calle Woodland 1281";
+}
